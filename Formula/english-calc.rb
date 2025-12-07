f385e9a8f08be65b621a80fcf6e0931f8894e8ca
Rihan227 <mohammad.shahzad@quintype.com>
2025-12-07 17:43:25 +0530

    Update sha256 checksum for english-calc

--- Formula/english-calc.rb
+++ Formula/english-calc.rb
@@ -2,7 +2,7 @@ class EnglishCalc < Formula
   desc "English-based calculator (e.g., 'five plus three')"
   homepage "https://github.com/Rihan227/english-calc"
   url "https://github.com/Rihan227/english-calc/archive/refs/tags/v1.0.0.zip"
-  sha256 "4c9e33f36343b5f5f06b6039777c53a2f01b047729cce1bb1672e179a48ff174"
+  sha256 "ad54be64366d02296ba4ae29285b3ccbecef24684de7fcea3e9e292f136346c8"
   license "MIT"
   version "1.0.0"
 
